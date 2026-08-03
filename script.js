fb4ce9d9f00ef50e1674d1fd8c210cb4fa2698f5
p2ttpb28hh-arch <p2ttpb28hh@privaterelay.appleid.com>
2026-08-03 18:34:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const intro = document.getElementById("intro");
+const scene = document.querySelector(".scene");
+const envelope = document.getElementById("envelope");
+const music = document.getElementById("music");
 
+intro.addEventListener("click", () => {
+
+    intro.style.opacity = "0";
+
+    setTimeout(() => {
+        intro.style.display = "none";
+        scene.style.opacity = "1";
+
+        if (music) {
+            music.play().catch(() => {});
+        }
+
+        setTimeout(() => {
+            envelope.classList.add("open");
+        }, 700);
+
+    }, 500);
+
+});
